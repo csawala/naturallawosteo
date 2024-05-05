f234c1f624801b379c0eff01d515a89e5615a520
Christopher Sawala <chris.sawala@gmail.com>
2024-05-05 19:10:12 -0400

    Basic first About route in place

---
+++ src/app/about/About.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <section>
+      <h1>ABOUT ASHLEY</h1>
+    </section>
+  )
+}
+
+export default About
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,14 +8,14 @@ const config: Config = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   plugins: [],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-      },
-    },
-  },
+  // theme: {
+  //   extend: {
+  //     backgroundImage: {
+  //       "gradient-conic":
+  //         "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+  //       "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+  //     },
+  //   },
+  // },
 };
 export default config;
