ea295faf3fc40a028cadce1adf41d13f65fb211c
Christopher Sawala <chris.sawala@gmail.com>
2024-05-05 20:14:58 -0400

    tailwind classes finally working

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <section className="p-6 flex flex-row w-full min-h-[80px] bg-white">
+      <h1 className="text-red-500">HEADER</h1>
+    </section>
+  )
+}
+
+export default Header
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,20 +2,11 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   plugins: [],
-  // theme: {
-  //   extend: {
-  //     backgroundImage: {
-  //       "gradient-conic":
-  //         "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-  //       "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-  //     },
-  //   },
-  // },
+  theme: {
+    extend: {},
+  },
 };
 export default config;
