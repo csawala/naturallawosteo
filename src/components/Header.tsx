a441ee5a508a3e06b7904f49fa6c99618e32ffea
Christopher Sawala <chris.sawala@gmail.com>
2024-05-21 20:10:50 -0400

    Added headless ui, and created the popover menu for smaller screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,12 @@
 
 import Image from "next/image"
 
+import {
+  Popover,
+  PopoverButton,
+  PopoverPanel,
+  Transition,
+} from "@headlessui/react"
 import { useRouter } from "next/navigation"
 import LogoWithLabel from "../../public/logoWithLabel.png"
 import Button from "./Button"
@@ -12,15 +18,18 @@ const Header = () => {
   const router = useRouter()
   const handleGoToBook = () => router.push("book")
   const handleGoToAbout = () => router.push("about")
+  const handleGoToHome = () => router.push("/")
 
   return (
     <section className={style.container}>
       <div className={style.header}>
-        <Image
-          alt="Natural Law Osteopathy"
-          className={style.logo}
-          src={LogoWithLabel}
-        />
+        <button onClick={handleGoToHome}>
+          <Image
+            alt="Natural Law Osteopathy"
+            className={style.logo}
+            src={LogoWithLabel}
+          />
+        </button>
         <div className={style.buttonContainer}>
           <div className={style.buttons}>
             <Button onClick={handleGoToAbout} variant="secondary">
@@ -36,9 +45,43 @@ const Header = () => {
           <Button onClick={handleGoToBook}>Book</Button>
 
           <div className={style.menu}>
-            <Button onClick={handleGoToAbout} variant="text">
-              <MenuIcon />
-            </Button>
+            <Popover className="relative items-center">
+              <PopoverButton className="items-center justify-center p-2">
+                <MenuIcon />
+              </PopoverButton>
+              <Transition
+                enter="transition-opacity duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity duration-150"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <PopoverPanel
+                  anchor={{ to: "top end", gap: 8 }}
+                  className="flex flex-col gap-y-2 bg-white rounded-md border"
+                >
+                  <button
+                    className="py-3 px-4 items-center justify-center text-sm"
+                    onClick={handleGoToAbout}
+                  >
+                    About
+                  </button>
+                  <button
+                    className="py-3 px-4 items-center justify-center text-sm"
+                    onClick={handleGoToAbout}
+                  >
+                    What is Osteopathy?
+                  </button>
+                  <button
+                    className="py-3 px-4 items-center justify-center text-sm"
+                    onClick={handleGoToAbout}
+                  >
+                    Frequently Asked Questions
+                  </button>
+                </PopoverPanel>
+              </Transition>
+            </Popover>
           </div>
         </div>
       </div>
