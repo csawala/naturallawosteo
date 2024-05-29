4f442e42e805d5a6fbee559c7da38fd294c0d85c
Christopher Sawala <chris.sawala@gmail.com>
2024-05-28 20:45:26 -0400

    Added Hero, and overlays, underlays, etc, but needs work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,46 @@
 "use client"
 
+import Image from "next/image"
 import { useRouter } from "next/navigation"
 
+import Button from "@/components/Button"
 import Card from "@/components/Card"
+import Hero from "../../public/Hero.jpg"
 import globalStyle from "./globalStyle.module.css"
+import style from "./styles/Home.module.css"
 
 export default function Home() {
   const router = useRouter()
   const handleGoToAbout = () => router.push("about")
+
   return (
     <main className={globalStyle.page}>
       <section>
-        <Card className="my-5">
+        <div className={style.heroContainer}>
+          <Image
+            alt="Ashley treating a patient"
+            // 1.76 ratio / 0.5666667
+            className={style.hero}
+            src={Hero}
+          />
+          <div className={style.heroOverlayFromBottom} />
+          <div className={style.heroOverlayFromTop} />
+          <div className={style.heroOverlayFromLeft} />
+          <div className={style.heroOverlayFromRight} />
+
+          <div className={style.heroHeaderUnderlay}>
+            <h1 className={style.heroHeader}>Treating What the Body Needs</h1>
+          </div>
+
+          <div className={style.heroFooterUnderlay}>
+            <h2 className={style.heroFooter}>You Want I Should Fix You Up?</h2>
+            <Button className={style.heroFooterCta} variant="outline">
+              Book an Appointment
+            </Button>
+          </div>
+        </div>
+
+        <Card className={style.card}>
           <div className="flex flex-col gap-y-3">
             <h1>NATURAL LAW OSTEOPATHY</h1>
             <p className="mb-4">
@@ -60,10 +89,6 @@ export default function Home() {
             </div>
           </div>
         </Card>
-
-        <button className="mt-6" onClick={handleGoToAbout}>
-          ABOUT
-        </button>
       </section>
     </main>
   )
