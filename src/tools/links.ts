a06ff40bdc177b24607e3c20a51c909c735f08aa
Christopher Sawala <chris.sawala@gmail.com>
2024-06-18 17:58:50 -0400

    Actually renaming file

---
+++ src/tools/links.ts
@@ -0,0 +1,15 @@
+export enum Links {
+  About = "/about",
+  Book = "/book",
+  FAQ = "/faq",
+  Home = "/",
+  WhatIsOsteo = "/osteo",
+}
+
+export const LinkName = {
+  [Links.About]: "About",
+  [Links.Book]: "Book",
+  [Links.FAQ]: "Frequently Asked Questions",
+  [Links.Home]: "Home",
+  [Links.WhatIsOsteo]: "What is Osteopathy?",
+}
