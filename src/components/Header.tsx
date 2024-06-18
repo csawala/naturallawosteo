93da86def744f39a8b4ba91cdbc2efece8ee0569
Christopher Sawala <chris.sawala@gmail.com>
2024-06-18 17:46:30 -0400

    Cleaning up styling. Header better fixed at bottom. Image still a problem, but getting there

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,83 +31,86 @@ const Header = () => {
 
   return (
     <nav className={style.container}>
-      <div className={style.header}>
-        <button onClick={handleGoToHome}>
-          <Image
-            alt="Natural Law Osteopathy"
-            className={style.logo}
-            src={LogoWithLabel}
-          />
-        </button>
-        <div className={style.buttonContainer}>
-          <div className={style.buttons}>
-            <Button onClick={handleGoToAbout} variant="secondary">
-              {LinkName[Links.About]}
-            </Button>
-            <Button onClick={handleGoToAbout} variant="outline">
-              {LinkName[Links.WhatIsOsteopathy]}
-            </Button>
-            <Button onClick={handleGoToAbout} variant="text">
-              {LinkName[Links.FAQ]}
-            </Button>
-          </div>
-          <Button onClick={handleGoToBook}>Book</Button>
+      <div className={style.innerContainer}>
+        <div className={style.header}>
+          <button onClick={handleGoToHome}>
+            <Image
+              alt="Natural Law Osteopathy"
+              className={style.logo}
+              src={LogoWithLabel}
+            />
+          </button>
+          <div className={style.buttonContainer}>
+            <div className={style.buttons}>
+              <Button onClick={handleGoToAbout} variant="secondary">
+                {LinkName[Links.About]}
+              </Button>
+              <Button onClick={handleGoToAbout} variant="outline">
+                {LinkName[Links.WhatIsOsteopathy]}
+              </Button>
+              <Button onClick={handleGoToAbout} variant="text">
+                {LinkName[Links.FAQ]}
+              </Button>
+            </div>
+            <Button onClick={handleGoToBook}>Book</Button>
 
-          <div className={style.menuContainer}>
-            <Popover className="relative">
-              <PopoverButton className="items-center justify-center p-2">
-                <MenuIcon />
-              </PopoverButton>
-              <PopoverOverlay className={style.menuOverlay} />
-              <Transition
-                enter="transition-opacity duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="transition-opacity duration-150"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <PopoverPanel
-                  anchor={{ to: "top end", gap: 8 }}
-                  className={style.menu}
+            <div className={style.menuContainer}>
+              <Popover className="relative">
+                <PopoverButton className="items-center justify-center p-2">
+                  <MenuIcon />
+                </PopoverButton>
+                <PopoverOverlay className={style.menuOverlay} />
+                <Transition
+                  enter="transition-opacity duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="transition-opacity duration-150"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
                 >
-                  {({ close }) => (
-                    <>
-                      <button
-                        className={clsx(
-                          style.menuButton,
-                          pathname === Links.About && style.menuButtonSelected,
-                        )}
-                        onClick={() => handleNavigate(close, Links.About)}
-                      >
-                        {LinkName[Links.About]}
-                      </button>
-                      <button
-                        className={clsx(
-                          style.menuButton,
-                          pathname === Links.WhatIsOsteopathy &&
-                            style.menuButtonSelected,
-                        )}
-                        onClick={() =>
-                          handleNavigate(close, Links.WhatIsOsteopathy)
-                        }
-                      >
-                        {LinkName[Links.WhatIsOsteopathy]}
-                      </button>
-                      <button
-                        className={clsx(
-                          style.menuButton,
-                          pathname === Links.FAQ && style.menuButtonSelected,
-                        )}
-                        onClick={() => handleNavigate(close, Links.FAQ)}
-                      >
-                        {LinkName[Links.FAQ]}
-                      </button>
-                    </>
-                  )}
-                </PopoverPanel>
-              </Transition>
-            </Popover>
+                  <PopoverPanel
+                    anchor={{ to: "top end", gap: 8 }}
+                    className={style.menu}
+                  >
+                    {({ close }) => (
+                      <>
+                        <button
+                          className={clsx(
+                            style.menuButton,
+                            pathname === Links.About &&
+                              style.menuButtonSelected,
+                          )}
+                          onClick={() => handleNavigate(close, Links.About)}
+                        >
+                          {LinkName[Links.About]}
+                        </button>
+                        <button
+                          className={clsx(
+                            style.menuButton,
+                            pathname === Links.WhatIsOsteopathy &&
+                              style.menuButtonSelected,
+                          )}
+                          onClick={() =>
+                            handleNavigate(close, Links.WhatIsOsteopathy)
+                          }
+                        >
+                          {LinkName[Links.WhatIsOsteopathy]}
+                        </button>
+                        <button
+                          className={clsx(
+                            style.menuButton,
+                            pathname === Links.FAQ && style.menuButtonSelected,
+                          )}
+                          onClick={() => handleNavigate(close, Links.FAQ)}
+                        >
+                          {LinkName[Links.FAQ]}
+                        </button>
+                      </>
+                    )}
+                  </PopoverPanel>
+                </Transition>
+              </Popover>
+            </div>
           </div>
         </div>
       </div>
