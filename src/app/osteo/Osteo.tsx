fea4067f3c54c810eadbc23c17103466f4ac6285
Christopher Sawala <chris.sawala@gmail.com>
2024-08-19 19:58:58 -0400

    Added image of AT Still and loosely styling osteo page

--- src/app/osteo/Osteo.tsx
+++ src/app/osteo/Osteo.tsx
@@ -2,6 +2,8 @@ import globalStyle from "@/app/globalStyle.module.css"
 import style from "@/app/osteo/styles/Osteo.module.css"
 import Banner from "@/components/Banner"
 import Card from "@/components/Card"
+import Image from "next/image"
+import ATStill from "../../../public/ATStill.jpg"
 
 const Quotes = [
   "The role of the osteopath is to help the body help itself",
@@ -10,6 +12,7 @@ const Quotes = [
   "Osteopathy is not a system, it is a truth",
   "Disease is the result of a violation of Natural Law",
 ]
+const RANDOM_QUOTE = Math.floor(Math.random() * Quotes.length)
 
 const Osteo = () => {
   return (
@@ -17,27 +20,40 @@ const Osteo = () => {
       <Card className={style.card}>
         <h1>What Is Osteopathy?</h1>
         <p className={style.paragraph}>
-          Osteopathy is a holistic approach to healthcare that focuses on
-          diagnosing, treating, and preventing musculoskeletal issues, as well
-          as providing holistic care for various other health conditions.
-          Osteopaths use manual techniques to restore balance within the body's
-          structure, promoting overall health and well-being. This practice is
-          based on the belief that the body has the inherent ability to heal
-          itself, and osteopathic treatment aims to support this natural healing
-          process. It emphasizes the interconnectedness of the body's structure
-          and function, recognizing that dysfunction in one area can impact
-          other areas of the body.
+          {
+            "Osteopathy is a holistic approach to healthcare that focuses on diagnosing, treating, and preventing musculoskeletal issues, as well as providing holistic care for various other health conditions. Osteopaths use manual techniques to restore balance within the body's structure, promoting overall health and well-being. This practice is based on the belief that the body has the inherent ability to heal itself, and osteopathic treatment aims to support this natural healing process. It emphasizes the interconnectedness of the body's structure and function, recognizing that dysfunction in one area can impact other areas of the body."
+          }
         </p>
       </Card>
-      <p>SPACER</p>
-      <Banner>
-        <span className="text-2xl">Enter quote here</span>
+
+      <Banner className={style.banner} variant="green">
+        <div className="flex flex-col">
+          <span className={style.quote}>{`"${Quotes[RANDOM_QUOTE]}"`}</span>
+          <span className={style.quoteAttribution}>- A.T. Still</span>
+        </div>
       </Banner>
+
+      <Card className={style.card}>
+        <h2>Andrew Taylor Still</h2>
+        <div>
+          <Image
+            alt="A.T. Still holding a femur"
+            className={style.image}
+            src={ATStill}
+          />
+          <p className={style.paragraph}>
+            {
+              "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut impedit ipsum explicabo vel! Aperiam aliquam, accusantium nostrum perspiciatis eius ipsam esse eum nobis est dolores eligendi, repellat, et error fugiat?"
+            }
+          </p>
+        </div>
+      </Card>
+
       <Banner className="mt-4" variant="purple">
-        <span className="text-2xl">Enter quote here</span>
+        <span className={style.quote}>Enter quote here</span>
       </Banner>
       <Banner className="mt-4" variant="green">
-        <span className="text-2xl">Enter quote here</span>
+        <span className={style.quote}>Enter quote here</span>
       </Banner>
     </section>
   )
