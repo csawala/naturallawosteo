a54898e4ba2bff252d998dd2e094276532ba8d4a
Christopher Sawala <chris.sawala@gmail.com>
2024-11-19 19:14:23 -0500

    Clean up FAQ styles

--- src/app/faq/Faq.tsx
+++ src/app/faq/Faq.tsx
@@ -9,6 +9,7 @@ import Button from "@/components/Button"
 import Card from "@/components/Card"
 import { Leaf } from "@/components/Icons"
 import { Links } from "@/tools/links"
+import style from "./styles/Faq.module.css"
 
 const Benefits = [
   "Improved circulation",
@@ -42,12 +43,12 @@ const Faq = () => {
         button: "What can Osteopathy help with?",
         panel: (
           <>
-            <p className="mb-2">
+            <p className={style.paragraph}>
               {
                 "Osteopathy is a highly beneficial treatment for many common medical concerns. It can help provide relief and, in some cases cure chronic physical ailments."
               }
             </p>
-            <p className="mb-2">
+            <p className={style.paragraph}>
               {
                 "Some commmon conditions that benefit from osteopathic treatment include back pain, joint problems, headaches, and digestive issues, to name a few."
               }
@@ -58,11 +59,11 @@ const Faq = () => {
       {
         button: "What are the benefits of Osteopathy?",
         panel: (
-          <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <ul className={style.listContainer}>
             {Benefits.map((benefit) => (
-              <li className="flex flex-row items-center" key={benefit}>
+              <li className={style.listItem} key={benefit}>
                 <Leaf
-                  className="rotate-90 mr-2"
+                  className={style.listIcon}
                   fill="primaryDark"
                   size="xsmall"
                 />
@@ -76,7 +77,7 @@ const Faq = () => {
         button: "What does a Manual Osteopath need to know?",
         panel: (
           <>
-            <p className="mb-2">
+            <p className={style.paragraph}>
               {
                 "Click below to get a glimpse into what Manual Osteopaths need to know before graduating from the Canadian Academy of Osteopathy."
               }
@@ -91,17 +92,17 @@ const Faq = () => {
         button: "What should I expect during my appointment?",
         panel: (
           <>
-            <p className="mb-2">
+            <p className={style.paragraph}>
               {
                 "Each treatment will always consider motion restriction and asymmetries in the body, with the goal of facilitating normal movement. This means that your visit will include a dynamic assessment of the way your body moves, followed by a treatment plan designed specifically for you and the current needs of your body."
               }
             </p>
-            <p className="mb-2">
+            <p className={style.paragraph}>
               {
                 "Treatment will include joint mobilization and soft tissue treatment to remove any obstacles that interfere with the proper nutrition and drainage of your body’s tissue. It is through a deep understanding of anatomy and physiology, and with excellent palpation skills, that an osteopathic manual therapist is able to detect and treat these restrictions."
               }
             </p>
-            <p className="mb-2">
+            <p className={style.paragraph}>
               {
                 "This approach to manual therapy aims to treat the cause of the symptom rather than simply reducing it temporarily."
               }
@@ -113,12 +114,12 @@ const Faq = () => {
         button: "Are osteopathic treatments painful?",
         panel: (
           <>
-            <p className="mb-2">
+            <p className={style.paragraph}>
               {
                 "Everyone reacts differently to treatment. In some cases, you may be sore for a couple days afterwards, similar to muscle soreness you experience after a workout."
               }
             </p>
-            <p className="mb-2">
+            <p className={style.paragraph}>
               {
                 "The treatment itself is designed to be gentle and comfortable, working in harmony with your body to ease the nervous system. This approach promotes relaxation while enhancing mobility and function, with a strong focus on patient comfort."
               }
@@ -130,17 +131,17 @@ const Faq = () => {
         button: "Are there any risks or side effects?",
         panel: (
           <>
-            <p className="mb-2">
+            <p className={style.paragraph}>
               {
                 "Yes, while osteopathic treatment is generally safe when performed by a qualified practitioner, there are some potential risks and side effects. Common, mild side effects include temporary soreness, fatigue, or slight bruising, which typically resolve within a day or two. In rare cases, people might experience dizziness, headaches, or muscle stiffness shortly after treatment."
               }
             </p>
-            <p className="mb-2">
+            <p className={style.paragraph}>
               {
                 "More serious complications, such as nerve or blood vessel damage, are extremely rare, especially when proper screening and gentle techniques are used. Patients with certain conditions—like osteoporosis, bone or joint infections, or recent fractures—should discuss their health with their osteopath to ensure a tailored, safe approach."
               }
             </p>
-            <p className="mb-2">
+            <p className={style.paragraph}>
               {
                 "Overall, osteopaths prioritize gentle techniques to minimize risk and maximize patient safety, particularly for those with specific health concerns."
               }
@@ -153,12 +154,12 @@ const Faq = () => {
           "Can osteopathic treatment be combined with other forms of medical care?",
         panel: (
           <>
-            <p className="mb-2">
+            <p className={style.paragraph}>
               {
                 "Yes, osteopathic treatment can be effectively combined with other forms of medical care. Osteopathy is often used as a complementary therapy alongside conventional medicine, physical therapy, or even mental health support. Many patients benefit from a holistic approach where osteopathy addresses physical alignment, mobility, and pain relief, while other treatments may target specific medical issues, rehabilitation needs, or lifestyle adjustments."
               }
             </p>
-            <p className="mb-2">
+            <p className={style.paragraph}>
               {
                 "Osteopaths frequently work in collaboration with other healthcare providers to ensure that treatments align and to support overall health goals. Open communication between your osteopath and other practitioners can help create a well-rounded, coordinated care plan tailored to your unique needs."
               }
@@ -175,17 +176,17 @@ const Faq = () => {
         button: "How many treatments will I need?",
         panel: (
           <>
-            <p className="mb-2">
+            <p className={style.paragraph}>
               {
                 "Every patients situation is unique and will impact the amount of treatments required. Factors include how long the issue has persisted, other traumas they've experienced, and the patients overall health."
               }
             </p>
-            <p className="mb-2">
+            <p className={style.paragraph}>
               {
                 "Regardless of the issue, patients can expect to notice changes within a short time frame (3 to 5 treatments)."
               }
             </p>
-            <p className="mb-2">
+            <p className={style.paragraph}>
               {
                 "Patients may often see complete restoration of function within a few visits. Others may require a prolonged approach (10 to 15 treatments), while a few may require a maintenance program."
               }
@@ -202,12 +203,12 @@ const Faq = () => {
         button: "What should I wear?",
         panel: (
           <>
-            <p className="mb-2">
+            <p className={style.paragraph}>
               {
                 "Comfort is key for osteopathy. Since you'll be getting moved around it's best to wear loose, comfortable clothing, like workout clothes."
               }
             </p>
-            <p className="mb-2">
+            <p className={style.paragraph}>
               {
                 "Avoid wearing restrictive clothing like jeans, or clothing that may shift during movement like skirts. You will be asked to removed any unnecessary accessories before treatment, such as hats and jewellery."
               }
@@ -226,7 +227,7 @@ const Faq = () => {
 
   return (
     <section className={globalStyle.page}>
-      <Card className="flex flex-col w-full">
+      <Card className={style.card}>
         <h1>Frequently Asked Questions</h1>
         <Accordian className="mt-4" content={Questions} />
       </Card>
